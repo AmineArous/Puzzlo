68e3370e1a01aaf8afdb00bb51d93ea50c4ea9b9
aarous-accor <124670344+aarous-accor@users.noreply.github.com>
2023-03-29 09:14:47 +0200

    Initial Commit

---
+++ BlocksGame/BlocksGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlocksGameApp.swift
+//  BlocksGame
+//
+//  Created by Amine Arous on 29/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct BlocksGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
